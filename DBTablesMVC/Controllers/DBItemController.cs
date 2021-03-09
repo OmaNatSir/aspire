06b6c73e0219bd07c190e5cc9f1f1c3e9a7a3b17
OmaNatSir <natalia.siren@elisanet.fi>
2021-03-09 09:04:52 +0200

    Added _DisplayTableListPartial.cshtml, which is used in GetDatabaseDetails.cshtml and GetColumnList.cshtml

--- DBTablesMVC/Controllers/DBItemController.cs
+++ DBTablesMVC/Controllers/DBItemController.cs
@@ -22,37 +22,52 @@ namespace DBTablesMVC.Controllers
         [HttpGet("database/{databaseName}")]
         public IActionResult GetDatabaseDetails(string databaseName)
         {
-            var database = _dbContext.DldDatabase.Where(dbs => dbs.DatabaseName == databaseName).Single();
+            try
+            {
+                var database = _dbContext.DldDatabase.Where(dbs => dbs.DatabaseName == databaseName).Single();
 
-            return View((DldDatabase)database);
-        }
+                ViewBag.ListTable = GetTableList(databaseName);
+                ViewBag.DatabaseName = database.DatabaseName;
 
+                return View((DldDatabase)database);
+            }
+            catch
+            {
+                return Content("Error: there is no database with such name");
+            }
 
-        //TEST: /database/adventureworkslt2017/table
-        [HttpGet("database/{databaseName}/table")]
-        public IActionResult GetTableList(string databaseName)
-        {
-            List<DldTable> listTable = _dbContext.DldTable.Include(table => table.DldSchema)
-                                         .ThenInclude(schema => schema.DldDatabase)
-                                         .Where(tbl => tbl.DldSchema.DldDatabase.DatabaseName == databaseName)
-                                         .ToList();
-
-            return View(listTable);
         }
 
         //TEST: /database/adventureworkslt2017/schema/saleslt/table/address
         [HttpGet("database/{databaseName}/schema/{schemaName}/table/{tableName}")]
         public IActionResult GetColumnList(string databaseName, string schemaName, string tableName)
         {
-            List<DldColumn> listColumn = _dbContext.DldColumn.Include(clm => clm.DldTable)
+            List<DldColumn> listColumn = GetColumnListDetails(databaseName, schemaName, tableName);
+
+            ViewBag.ListTable = GetTableList(databaseName);
+            ViewBag.TableName = listColumn[0].DldTable.TableName;   //Ensuring capital letters in {tableName}
+            ViewBag.DatabaseName = listColumn[0].DldTable.DldSchema.DldDatabase.DatabaseName; // in {databaseName}
+
+            return View(listColumn);
+        }
+
+        private List<DldTable> GetTableList(string databaseName)
+        {
+            return _dbContext.DldTable.Include(table => table.DldSchema)
+                                         .ThenInclude(schema => schema.DldDatabase)
+                                         .Where(tbl => tbl.DldSchema.DldDatabase.DatabaseName == databaseName)
+                                         .ToList();
+        }
+
+        private List<DldColumn> GetColumnListDetails(string databaseName, string schemaName, string tableName)
+        {
+            return _dbContext.DldColumn.Include(clm => clm.DldTable)
                                         .ThenInclude(tbl => tbl.DldSchema)
                                         .ThenInclude(scm => scm.DldDatabase)
                                         .Where(clm => clm.DldTable.DldSchema.DldDatabase.DatabaseName == databaseName)
                                         .Where(clm => clm.DldTable.DldSchema.SchemaName == schemaName)
                                         .Where(clm => clm.DldTable.TableName == tableName)
                                         .ToList();
-
-            return View(listColumn);
         }
     }
 }
